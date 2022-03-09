f71f2e5ae3a98f20438939d08b99286b9a4c8232
Luiz Fernando <96695552+Luizfernando022@users.noreply.github.com>
2022-03-09 19:14:52 -0300

    ?

--- js.js
+++ js.js
@@ -10,10 +10,34 @@ btn.addEventListener('click',()=>{
     let adulto = document.getElementById('i2').value
     let duracao = document.getElementById('i3').value
     if(crianca === "" && adulto === ""){
-        console.log("Preencha algum campo")
+        document.getElementById('erro').innerHTML = "<p>Informe a quantidade de pessoas!!"
+        document.getElementById('erro').style.color = "red"
+        document.getElementById('erro').style.textShadow = "1px 1px 10px"
     }else{
 
-        let quantidadeCarne = 400 * adulto + (crianca * 200)
+        let quantidadeCarne = carne(duracao) * adulto + (crianca * carne(duracao)/2)
+        let quantidadeAgua = adulto + crianca + 2 
+        document.getElementById('calc').style.animationName = "calcAnimationOut"
+        document.getElementById('result').style.display = "block"
+        document.getElementById('calc').style.display = "none"
+        document.getElementById('result').style.animationName = "resultAnimation"
+        console.log(quantidadeCarne)
+        console.log(duracao)
+        function carne(e){
+            if(e >= 8){
+                return 800
+            }
+            else if(e >= 3){
+                return 600
+            }
+            else if(e >= 1){
+                return 400
+            }
+            else{
+                return 400
+            }
+            
+        }
     }
 
    
